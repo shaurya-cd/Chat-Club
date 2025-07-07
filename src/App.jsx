a868367c3f6331205f131451a7c2f7c92b4758e3
shaurya <2447shaurya48@gmail.com>
2025-07-07 11:18:35 +0530

    feat: update project structure and add routing
    - Changed the title in index.html to "Chat Club".
    - Added axios and zustand to dependencies in package.json and package-lock.json.
    - Introduced autoprefixer and daisyui in devDependencies.
    - Created postcss.config.js for Tailwind CSS and autoprefixer integration.
    - Updated App component to include Navbar and routing for Home, SignUp, Login, Settings, and Profile pages.
    - Removed unused react.svg asset.
    - Created Navbar component.
    - Added Home, Login, Profile, Settings, and SignUp pages.
    - Updated main.jsx to wrap App component with BrowserRouter.
    - Updated tailwind.config.js to include daisyui and adjust content paths.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
+import Settings from './pages/Settings'
+import Profile from './pages/Profile'
+
 
 function App() {
   return (
-    <div className='text-red-500'>App</div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/settings' element={<Settings />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
- /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
